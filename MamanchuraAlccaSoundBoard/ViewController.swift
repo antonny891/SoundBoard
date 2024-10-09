3e337b0012266b5729143ecc04fba28aa186e9f9
antonny <melber.mamanchura@tecsup.edu.pe>
2024-10-09 18:22:15 -0500

    Initial Commit

---
+++ MamanchuraAlccaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MamanchuraAlccaSoundBoard
+//
+//  Created by Antonny Mmamanchura Alcca on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
